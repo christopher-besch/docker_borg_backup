ac24f01c6464688c55b6fb05bb93f9651857a60b
christopher-besch <mail@chris-besch.com>
2022-10-23 08:21:36 +0200

    creating repo if not existent

--- borg_backup_init.sh
+++ borg_backup_init.sh
@@ -6,9 +6,16 @@
 set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+REPO="/repo"
 
 # TODO: allow use without cron and SIGHUP
 echo "init at $(date)"
+
+if [ -z "$(ls -A $REPO)" ]; then
+   echo "$REPO is empty, creating borg repo"
+   borg -r $REPO rcreate --encryption=none
+fi
+
 # call script when receiving SIGHUP
 # set -e exits script after trap
 set +e
